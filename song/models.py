0f82df5a0786dd776f65f830bc779f866eb44ba8
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 10:22:34 -0500

    Created song app/model & migrated to database.

---
+++ song/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    release_date = models.DateField(null=True, blank=False)
+    genre = models.CharField(max_length=100)
