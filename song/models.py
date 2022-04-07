942a581ab6fd760d07c26cec446f3927258ede3c
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 13:37:39 -0500

    Add likes field to song model and serializer.

--- song/models.py
+++ song/models.py
@@ -7,3 +7,4 @@ class Song(models.Model):
     album = models.CharField(max_length=100)
     release_date = models.DateField(null=True, blank=False)
     genre = models.CharField(max_length=100)
+    likes = models.IntegerField(default=0)
