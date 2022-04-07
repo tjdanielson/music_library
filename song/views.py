4f63d0245a13c6b7e16b83484641e537df13cf5a
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 12:54:42 -0500

    Created put method in SongDetail class view to update a song by id.

--- song/views.py
+++ song/views.py
@@ -34,3 +34,11 @@ class SongDetail(APIView):
         song = self.get_object(pk)
         serializer = SongSerializer(song)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
