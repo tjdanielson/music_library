3956ac41e1f955ea6e62c75268ff116eb6c5eab6
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 11:05:21 -0500

    Added serializer for song model, created SongList view with skeleton of get and post requests.

--- song/views.py
+++ song/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class SongList(APIView):
+
+    def get(self, request):
+        pass
+
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
